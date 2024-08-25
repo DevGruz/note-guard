8dd28b9c0dec732f8e8bbf8c1dbe89b327fa7719
Ivan Gruzdev <gruzdev.ivan.a@gmail.com>
2024-08-25 20:50:43 +0300

    Sql query output to the console is disabled

--- app/core/db.py
+++ app/core/db.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_asyn
 from app.core.config import settings
 
 
-async_engine = create_async_engine(url=settings.DATABASE.URL, echo=True)
+async_engine = create_async_engine(url=settings.DATABASE.URL)
 async_session_maker = async_sessionmaker(async_engine, expire_on_commit=False)
 
 
